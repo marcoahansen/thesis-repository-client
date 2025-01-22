67b6690d202017d40d8fde2a55667519d1731e41
marcoahansen <m.hansendeoliveira@gmail.com>
2025-01-22 11:45:39 -0300

    feat: remove explicit Content-Type header from Axios instance

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -5,7 +5,4 @@ const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 export const api = axios.create({
   baseURL: BACKEND_URL ?? "http://localhost:3333/api",
   withCredentials: true,
-  headers: {
-    "Content-Type": "application/json",
-  },
 });
