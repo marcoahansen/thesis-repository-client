9b8b4675b48351e8022b89c3a38abf158165fd0b
marcoahansen <m.hansendeoliveira@gmail.com>
2025-01-22 08:47:39 -0300

    feat: set default Content-Type header to application/json in Axios instance

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -5,4 +5,7 @@ const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 export const api = axios.create({
   baseURL: BACKEND_URL ?? "http://localhost:3333/api",
   withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
